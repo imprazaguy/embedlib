3bf219b6fdb76466cf03037534c4ac20ec49d1f6
Kuan-Chung Huang <imprazaguy@realtek.com>
2016-03-11 18:25:03 +0800

    rbtree: Add compact version of red black tree library

---
+++ rbtree/rbtree_compact.c
@@ -0,0 +1,211 @@
+#include "rbtree_compact.h"
+#include <assert.h>
+
+
+RB_NODE *rb_rotate(RB_NODE *node, int direction)
+{
+    RB_NODE *pivot = rb_child(node, direction ^ 1);
+    rb_set_child(node, direction ^ 1, rb_child(pivot, direction));
+    rb_set_child(pivot, direction, node);
+    return pivot;
+}
+
+void rb_insert_color(RB_ROOT *root, RB_PATH *rp)
+{
+    RB_NODE *parent;
+    while ((parent = rp->cur->parent) != NULL && rb_color(parent) == RB_RED)
+    {
+        /* NOTE: gparent can't be NULL; otherwise, it implies parent is root
+         * and black, contradicting the loop condition.
+         */
+        RB_NODE *gparent = rp->cur[-1].parent;
+        int dir = rp->cur[-1].dir;
+        RB_NODE *uncle = rb_child(gparent, dir ^ 1);
+        if (uncle != NULL && rb_color(uncle) == RB_RED)
+        {
+            /* Case 1: Uncle is red.
+             */
+            rb_set_color(parent, RB_BLACK);
+            rb_set_color(uncle, RB_BLACK);
+            rb_set_color(gparent, RB_RED);
+            rp->cur -= 2;
+        }
+        else
+        {
+            if (rp->cur->dir != dir)
+            {
+                /* Case 2: Uncle is black and the path from grandparent to
+                 * child is not straight.
+                 */
+                parent = rb_rotate(parent, dir);
+                rb_set_child(gparent, dir, parent);
+            }
+            /* Case 3: Uncle is black and the path from grandparent to child
+             * is straight.
+             */
+            rb_set_color(parent, RB_BLACK);
+            rb_set_color(gparent, RB_RED);
+            RB_NODE *t = rb_rotate(gparent, dir ^ 1);
+            RB_NODE *ggparent = rp->cur[-2].parent;
+            if (ggparent != NULL)
+            {
+                rb_set_child(ggparent, rp->cur[-2].dir, t);
+            }
+            else
+            {
+                root->rb_root = t;
+            }
+            break;
+        }
+    }
+    rb_set_color(root->rb_root, RB_BLACK);
+}
+
+
+void rb_remove_color(RB_ROOT *root, RB_NODE *node, RB_PATH *rp)
+{
+    while ((node == NULL || rb_color(node) == RB_BLACK)
+            && node != root->rb_root)
+    {
+        RB_NODE *parent = rp->cur->parent;
+        int dir = rp->cur->dir;
+        RB_NODE *sibling = rb_child(parent, dir ^ 1);
+        /* Because the node deleted in rb_remove() must be black to go here,
+         * its original sibling cannot be NULL; otherwise, it violates the
+         * rules.
+         */
+        if (rb_color(sibling) == RB_RED)
+        {
+            /* Case 1: Sibling is red.
+             */
+            rb_set_color(sibling, RB_BLACK);
+            rb_set_color(parent, RB_RED);
+            RB_NODE *t = rb_rotate(parent, dir);
+            RB_NODE *gparent = rp->cur[-1].parent;
+            if (gparent != NULL)
+            {
+                rb_set_child(gparent, rp->cur[-1].dir, t);
+            }
+            else
+            {
+                root->rb_root = t;
+            }
+            rp->cur[1] = rp->cur[0];
+            rp->cur->parent = t;
+            rp->cur->dir = dir;
+            ++rp->cur;
+            sibling = rb_child(parent, dir ^ 1);
+        }
+        RB_NODE *nibling;
+        if ((nibling = rb_child(sibling, dir ^ 1)) == NULL
+                || rb_color(nibling) == RB_BLACK)
+        {
+            if ((nibling = rb_child(sibling, dir)) == NULL
+                    || rb_color(nibling) == RB_BLACK)
+            {
+                /* Case 2: Sibling is black and both its children are black.
+                */
+                rb_set_color(sibling, RB_RED);
+                node = parent;
+                --rp->cur;
+                continue;
+            }
+
+            /* Case 3: Sibling is black and its child in the same postition
+             * as node is red and its child in the opposite position as
+             * node is black.
+             */
+            rb_set_color(rb_child(sibling, dir), RB_BLACK);
+            assert(rb_child(sibling, dir) != NULL);
+            rb_set_color(sibling, RB_RED);
+            RB_NODE *t = rb_rotate(sibling, dir ^ 1);
+            rb_set_child(parent, dir ^ 1, t);
+            sibling = t;
+        }
+
+        /* Case 4:
+        */
+        rb_set_color(sibling, rb_color(parent));
+        rb_set_color(parent, RB_BLACK);
+        rb_set_color(rb_child(sibling, dir ^ 1), RB_BLACK);
+        assert(rb_child(sibling, dir ^ 1) != NULL);
+        RB_NODE *t = rb_rotate(parent, dir);
+        RB_NODE *gparent = rp->cur[-1].parent;
+        if (gparent != NULL)
+        {
+            rb_set_child(gparent, rp->cur[-1].dir, t);
+        }
+        else
+        {
+            root->rb_root = t;
+        }
+        node = root->rb_root;
+        break;
+    }
+    if (node != NULL)
+    {
+        rb_set_color(node, RB_BLACK);
+    }
+}
+
+void rb_remove(RB_ROOT *root, RB_NODE *node, RB_PATH *rp)
+{
+    RB_NODE *replacement;
+    RB_NODE *transplanter;
+    RB_NODE *node_parent = rp->cur->parent;
+    int node_dir = rp->cur->dir;
+    int deleted_color = rb_color(node);
+    if (rb_child(node, RB_LEFT) == NULL) /* node has at most one right child */
+    {
+        transplanter = rb_child(node, RB_RIGHT);
+        replacement = transplanter;
+    }
+    else if (rb_child(node, RB_RIGHT) == NULL) /* node has one left child */
+    {
+        transplanter = rb_child(node, RB_LEFT);
+        replacement = transplanter;
+    }
+    else /* node has two children */
+    {
+        RB_PATH_ENTRY *node_rpe = ++rp->cur;
+        rp->cur->parent = node;
+        rp->cur->dir = RB_RIGHT;
+        RB_NODE *successor = rb_child(node, RB_RIGHT);
+        RB_NODE *left;
+        while ((left = rb_child(successor, RB_LEFT)) != NULL)
+        {
+            ++rp->cur;
+            rp->cur->parent = successor;
+            rp->cur->dir = RB_LEFT;
+            successor = left;
+        }
+        deleted_color = rb_color(successor);
+        RB_NODE *child = rb_child(successor, RB_RIGHT);
+        replacement = child;
+
+        RB_NODE *parent = rp->cur->parent; /* successor's parent */
+        if (parent != node)
+        {
+            rb_set_child(parent, rp->cur->dir, child);
+            rb_set_child(successor, RB_RIGHT, rb_child(node, RB_RIGHT));
+        }
+        rb_set_child(successor, RB_LEFT, rb_child(node, RB_LEFT));
+        rb_set_color(successor, rb_color(node));
+        node_rpe->parent = successor;
+        transplanter = successor;
+    }
+    if (node_parent != NULL)
+    {
+        rb_set_child(node_parent, node_dir, transplanter);
+    }
+    else
+    {
+        root->rb_root = transplanter;
+    }
+
+    if (deleted_color == RB_BLACK)
+    {
+        rb_remove_color(root, replacement, rp);
+    }
+}
+
---
+++ rbtree/rbtree_compact.h
@@ -0,0 +1,173 @@
+#ifndef RBTREE_COMPACT_H_
+#define RBTREE_COMPACT_H_
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#define RB_RED      0
+#define RB_BLACK    1
+
+#define RB_LEFT     0
+#define RB_RIGHT    1
+
+typedef struct RB_NODE_
+{
+    intptr_t rb_child_color[2];
+} RB_NODE;
+
+typedef struct RB_ROOT_
+{
+    RB_NODE *rb_root;
+} RB_ROOT;
+
+#define RB_ROOT_INITIALIZER(root) { NULL }
+#define RB_ROOT_INIT(root) do { (root)->rb_root = NULL; } while (0)
+
+#define RB_ENTRY(node, type, field) \
+    ((type *)((char *)(node) - offsetof(type, field)))
+
+static inline RB_NODE *rb_child(RB_NODE *node, int dir)
+{
+    return (RB_NODE *)(node->rb_child_color[dir] & ~0x1);
+}
+
+static inline void rb_set_child(RB_NODE *node, int dir, RB_NODE *child)
+{
+    node->rb_child_color[dir] = (((intptr_t) child) | (node->rb_child_color[dir] & 0x1));
+}
+
+static inline int rb_color(RB_NODE *node)
+{
+    return (node->rb_child_color[RB_RIGHT] & 0x1);
+}
+
+static inline void rb_set_color(RB_NODE *node, int color)
+{
+    node->rb_child_color[RB_RIGHT] = ((node->rb_child_color[RB_RIGHT] & ~0x1) | color);
+}
+
+typedef struct RB_PATH_ENTRY_
+{
+    RB_NODE *parent;
+    int dir;
+} RB_PATH_ENTRY;
+
+typedef struct RB_PATH_
+{
+    RB_PATH_ENTRY path[sizeof(void *) * 8];
+    RB_PATH_ENTRY *cur;
+} RB_PATH;
+
+#define RB_PATH_INIT(rp) \
+    do { (rp)->cur = (rp)->path; (rp)->cur->parent = NULL; } while(0)
+
+void rb_insert_color(RB_ROOT *root, RB_PATH *rp);
+void rb_remove(RB_ROOT *root, RB_NODE *node, RB_PATH *rp);
+
+#define RB_INSERT(name, root, node) name##_rb_insert(root, node)
+#define RB_REMOVE(name, root, key) name##_rb_remove(root, key)
+#define RB_FIND(name, root, key, rp) name##_rb_find(root, key, rp)
+
+#define RB_GENERATE_INSERT_PROTO(name, type) \
+type *name##_rb_insert(RB_ROOT *root, type *node)
+#define RB_GENERATE_INSERT(name, type, field, cmp) \
+RB_GENERATE_INSERT_PROTO(name, type) \
+{ \
+    RB_PATH rp; \
+    RB_PATH_INIT(&rp); \
+    RB_NODE *p = root->rb_root; \
+    if (p != NULL) \
+    { \
+        int dir; \
+        do \
+        { \
+            type *ent = RB_ENTRY(p, type, field); \
+            int c = cmp(node, ent); \
+            if (c < 0) \
+            { \
+                dir = RB_LEFT; \
+            } \
+            else if (c > 0) \
+            { \
+                dir = RB_RIGHT; \
+            } \
+            else \
+            { \
+                return ent; \
+            } \
+            ++rp.cur; \
+            rp.cur->parent = p; \
+            rp.cur->dir = dir; \
+            p = rb_child(p, dir); \
+        } while (p != NULL);\
+        rb_set_child(rp.cur->parent, dir, &node->field); \
+    } \
+    else \
+    { \
+        root->rb_root = &node->field; \
+    } \
+    rb_set_color(&node->field, RB_RED); \
+    rb_set_child(&node->field, RB_LEFT, NULL); \
+    rb_set_child(&node->field, RB_RIGHT, NULL); \
+    rb_insert_color(root, &rp); \
+    return node; \
+}
+
+#define RB_GENERATE_FIND_PROTO(name, key_type, type) \
+type *name##_rb_find(RB_ROOT *root, key_type key, RB_PATH *rp)
+#define RB_GENERATE_FIND(name, key_type, type, field, key_cmp) \
+RB_GENERATE_FIND_PROTO(name, key_type, type) \
+{ \
+    RB_PATH_INIT(rp); \
+    RB_NODE *p = root->rb_root; \
+    while (p != NULL) \
+    { \
+        int dir; \
+        type *ent = RB_ENTRY(p, type, field); \
+        int c = key_cmp(key, ent); \
+        if (c < 0) \
+        { \
+            dir = RB_LEFT; \
+        } \
+        else if (c > 0) \
+        { \
+            dir = RB_RIGHT; \
+        } \
+        else \
+        { \
+            return ent; \
+        } \
+        ++rp->cur; \
+        rp->cur->parent = p; \
+        rp->cur->dir = dir; \
+        p = rb_child(p, dir); \
+    } \
+    return NULL; \
+}
+
+#define RB_GENERATE_REMOVE_PROTO(name, key_type, type) \
+type *name##_rb_remove(RB_ROOT *root, key_type key)
+#define RB_GENERATE_REMOVE(name, key_type, type, field) \
+RB_GENERATE_REMOVE_PROTO(name, key_type, type) \
+{ \
+    RB_PATH rp; \
+    type *node = RB_FIND(name, root, key, &rp); \
+    if (node != NULL) \
+    { \
+        rb_remove(root, &node->field, &rp); \
+    } \
+    return node; \
+}
+
+#define RB_GEN_PROTO(name, key_type, type) \
+RB_GENERATE_INSERT_PROTO(name, type); \
+RB_GENERATE_REMOVE_PROTO(name, key_type, type); \
+RB_GENERATE_FIND_PROTO(name, key_type, type);
+
+#define RB_GEN(name, key_type, type, field, key_cmp, cmp) \
+RB_GENERATE_INSERT(name, type, field, cmp) \
+RB_GENERATE_FIND(name, key_type, type, field, key_cmp) \
+RB_GENERATE_REMOVE(name, key_type, type, field)
+
+#endif /* RBTREE_COMPACT_H_ */
